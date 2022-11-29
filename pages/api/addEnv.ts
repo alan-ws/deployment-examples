d81c8d4d993922148c0f4b06120668464f32c5b3
Alan Williams <alan.williams@vercel.com>
2022-11-29 09:27:06 +0200

    attempt deployment with API

---
+++ pages/api/addEnv.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const envURL = new URL(
+    `/v10/projects/${process.env.PROJECT_ID}/env`,
+    "https://api.vercel.com"
+  );
+
+  try {
+    const urlRes = await fetch(envURL, {
+      body: JSON.stringify({
+        key: "NEXT_PUBLIC_DYNAMIC_KEY",
+        value: 23,
+        type: "encrypted",
+        target: "preview",
+      }),
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(await urlRes.json());
+    res.status(200).json(await urlRes.json());
+  } catch (err) {
+    console.error(err);
+    res.status(200).json({ messages: `${err}` });
+  }
+}
---
+++ pages/api/deploy.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const deploymentURL = new URL(
+    `/v13/deployments?teamId=${process.env.TEAM_ID}`,
+    "https://api.vercel.com"
+  );
+  try {
+    const depURL = await fetch(deploymentURL, {
+      body: JSON.stringify({ name: process.env.NAME }),
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(await depURL.json());
+    res.status(200).json(await depURL.json());
+  } catch (err) {
+    console.error(err);
+    res.status(200).json({ message: `${err}` });
+  }
+}
