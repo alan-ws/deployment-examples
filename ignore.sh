538433550a48329f9211588a94b957ee4f94fbbc
Alan Williams <alan.williams@vercel.com>
2022-12-05 19:08:30 +0200

    now we should not deploy

--- ignore.sh
+++ ignore.sh
@@ -1,7 +1,9 @@
-if [[ "$VERCEL_GIT_COMMIT_REF" == "no_deploy" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" != "no_deploy" ]]; then
+  echo "on branch: $VERCEL_GIT_COMMIT_REF";
   echo 'GO';
   exit 1;
 fi
 
+echo "on branch: $VERCEL_GIT_COMMIT_REF";
 echo 'EXIT';
 exit 0;
